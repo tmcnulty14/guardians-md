8061c7c5b91f1a87769c26a1a7608b8e061758fa
creid1 <creid1@student.framingham.edu>
2015-04-13 18:27:13 -0400

    Commented out the @Before annotation

--- guardians_md/src/test/java/com/guardiansofthegalaxy/guardians_md/paneltests/SearchPanelJUnitTest.java
+++ guardians_md/src/test/java/com/guardiansofthegalaxy/guardians_md/paneltests/SearchPanelJUnitTest.java
@@ -1,15 +1,13 @@
 package com.guardiansofthegalaxy.guardians_md.paneltests;
 
 import com.guardiansofthegalaxy.guardians_md.db.MedicalConfigurator;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Created by Christina on 4/12/2015.
  */
 public class SearchPanelJUnitTest {
 
-    @Before
+  //  @Before
     public void beforeClass() {
         MedicalConfigurator.setActivePatient("Charles", "Henderson", "July 18, 2010", "M",
                 "13 Hill Lane", "", "Natick", "MA", "01744", "United States",
