00b6b5e790edb6eebd68a9d9dead0c932e6c1818
tmcnulty14 <tmcnulty@student.framingham.edu>
2015-04-21 18:12:37 -0400

    S3ImageStorage Class stub
    This class will be used for storing and accessing image results from an
    AWS S3 server.

---
+++ guardians_md/src/main/java/com/guardiansofthegalaxy/guardians_md/db/S3ImageStorage.java
@@ -0,0 +1,11 @@
+import java.awt.Image;
+
+public class S3ImageStorage {
+	public boolean storeImage(File image, String key) {
+		return false;
+	}
+
+	public Image getImage(String key) {
+		return null;
+	}
+}
