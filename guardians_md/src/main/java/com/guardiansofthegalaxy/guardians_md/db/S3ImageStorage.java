b4c89393980c204b60239b22b6578fa25e1dd0d3
tmcnulty14 <tmcnulty@student.framingham.edu>
2015-04-23 00:44:42 -0400

    S3 Image Storage Functionality
    Added functionality to store, retrieve, and delete images on an S3
    cloud bucket. This can be accessed via an instance of the
    S3ImageStorage class.
    
    This commit also includes a test class (S3Test) for the image storage.
    
    I also had to add a couple of dependencies to the maven project to make
    this work, primarily the AWS SDK dependency. You may notice Maven
    downloading more artifacts now when it builds/packages.

--- guardians_md/src/main/java/com/guardiansofthegalaxy/guardians_md/db/S3ImageStorage.java
+++ guardians_md/src/main/java/com/guardiansofthegalaxy/guardians_md/db/S3ImageStorage.java
@@ -2,13 +2,89 @@ package com.guardiansofthegalaxy.guardians_md.db;
 
 import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class S3ImageStorage {
-	public boolean storeImage(File image, String key) {
-		return false;
+	private static final String ACCESS_KEY_ID="AKIAIJIDR2QRDSKWXDLA";
+	private static final String SECRET_ACCESS_KEY="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";
+	private static final String BUCKET_NAME="guardians-md-lab-pictures";
+
+	private final AmazonS3 s3Client;
+
+	public S3ImageStorage() {
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+		s3Client = new AmazonS3Client(awsCreds);
 	}
 
-	public Image getImage(String key) {
-		return null;
+	/**
+	 * Stores an image at the specified file under the specified keyName.
+	 * @param image The File where the image is stored.
+	 * @param keyName The name to upload the image to S3 under.
+	 **/
+	public boolean storeImage(File image, String keyName) {
+		try {
+            s3Client.putObject(new PutObjectRequest(BUCKET_NAME, keyName, image));
+            return true;
+        } catch (AmazonServiceException ase) {
+            System.out.println("AmazonServerException: Error on S3 Server.");
+        } catch (AmazonClientException ace) {
+            System.out.println("AmazonClientException: Could not reach S3 server.");
+        }
+        return false;
+	}
+
+	/**
+	 * Downloads an image with the given key from the AmazonS3 bucket.
+	 * @param keyName The key used to pull the image from S3.
+	 * @return An Image object.
+	 **/
+	public Image getImage(String keyName) {
+		try {
+			S3Object object = s3Client.getObject(new GetObjectRequest(BUCKET_NAME, keyName));
+			Image image = ImageIO.read(object.getObjectContent());
+			return image;
+		} catch(IOException ioe) {
+			System.out.println("IOException while reading image from input stream.");
+			return null;
+		} catch (AmazonServiceException ase) {
+            System.out.println("AmazonServerException: Error on S3 Server.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("AmazonClientException: Could not reach S3 server.");
+        }
+        return null;
+	}
+
+	/**
+	 * Deletes an image with the given key from the AmazonS3 bucket.
+	 * @param keyName The key used to delete the image from S3.
+	 * @return true If deleted successfully.
+	 **/
+	public boolean deleteImage(String keyName) {
+		try {
+            s3Client.deleteObject(new DeleteObjectRequest(BUCKET_NAME, keyName));
+            return true;
+        } catch (AmazonServiceException ase) {
+            System.out.println("AmazonServerException: Error on S3 Server.");
+        } catch (AmazonClientException ace) {
+            System.out.println("AmazonClientException: Could not reach S3 server.");
+        }
+        return false;
 	}
 }
---
+++ guardians_md/src/test/java/com/guardiansofthegalaxy/guardians_md/dbtests/S3Test.java
@@ -0,0 +1,50 @@
+package com.guardiansofthegalaxy.guardians_md.dbtests;
+
+import com.guardiansofthegalaxy.guardians_md.db.*;
+import com.guardiansofthegalaxy.guardians_md.environments.ConfigDirectory;
+
+import org.junit.*;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
+public class S3Test {
+	private static final String TEST_IMAGE = ConfigDirectory.getImageFileFromDirectory("Home_button.png");
+	private static final String TEST_KEY = "test.png";
+
+	private static S3ImageStorage s3Conn = null;
+
+	@BeforeClass
+	public static void connectToS3() {
+		s3Conn = new S3ImageStorage();
+	}
+
+	@Test
+	public void testImage() {
+		try {
+			URL url = S3Test.class.getClassLoader().getResource(TEST_IMAGE);
+			Image testImage = ImageIO.read(url); 
+			Assert.assertNotNull(testImage);
+
+			File testFile = new File(TEST_KEY);
+			FileUtils.copyURLToFile(url, testFile);
+
+			Assert.assertTrue(s3Conn.storeImage(testFile, TEST_KEY));
+			Image image = s3Conn.getImage(TEST_KEY);
+			Assert.assertNotNull(image);
+
+			// Due to the way equality of BufferedImages is handled, the redownloaded image will not be considered "equal".
+			// Therefore this assertion will fail.
+			//Assert.assertEquals(testImage, image);
+
+			Assert.assertTrue(s3Conn.deleteImage(TEST_KEY));
+		} catch(IOException ioe) {
+			Assert.fail();
+		}
+	}
+}
