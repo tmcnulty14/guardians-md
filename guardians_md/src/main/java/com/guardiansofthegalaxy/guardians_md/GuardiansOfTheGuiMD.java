59f2e9f3ddb615c6e76afa9ba8506bda4d79cf5d
tmcnulty14 <tmcnulty@student.framingham.edu>
2015-04-27 22:49:20 -0400

    Added Header comment to main class
    Is this everything that should be in the header? It still needs to be
    propagated to all of the other files in the project.

--- guardians_md/src/main/java/com/guardiansofthegalaxy/guardians_md/GuardiansOfTheGuiMD.java
+++ guardians_md/src/main/java/com/guardiansofthegalaxy/guardians_md/GuardiansOfTheGuiMD.java
@@ -1,14 +1,27 @@
+/**
+ * Class: CSCI 400: Special Topic in Computer Science - GUI Programming
+ * Instructor: Professor Jung
+ * Date: Spring 2015
+ * 
+ * Group: The Guardians of the GUI
+ * Group Members: Christina Reid
+ * 				  Sara Hakkoum
+ * 				  Thomas McNulty
+ * 				  Trevor Gorman
+ * 
+ * Assignment: Project 2 - Medical Doctor software
+ **/
 package com.guardiansofthegalaxy.guardians_md;
 
-import com.guardiansofthegalaxy.guardians_md.labtesttypes.LabName;
-import com.guardiansofthegalaxy.guardians_md.labtesttypes.TestName;
-import com.guardiansofthegalaxy.guardians_md.panels.*;
-import com.guardiansofthegalaxy.guardians_md.db.*;
+import com.guardiansofthegalaxy.guardians_md.panels.MedicalSystemsMainFrame;
 
-import java.util.ArrayList;
-
+/**
+ * The main class of the project. This class' main method is run when the packaged .jar is executed.
+ **/
 public class GuardiansOfTheGuiMD {
-
+	/**
+	 * Creates a new MedicalSystemsMainFrame, which then runs everything else in the project.
+	 **/
     public static void main(String[] args){
         MedicalSystemsMainFrame main = new MedicalSystemsMainFrame();
     }
